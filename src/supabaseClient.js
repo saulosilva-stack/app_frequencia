3955fdf9beb77417650af6df971019f14d3e8fc6
saulosilva-stack <saulo.marista@gmail.com>
2026-05-07 16:51:02 -0300

    supabaseClient refeito

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kmrzgnyyeaaiowaqmavu.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imttcnpnbnl5ZWFhaW93YXFtYXZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxMzYyOTUsImV4cCI6MjA5MTcxMjI5NX0.S74bdTjd0-QgOxShSrdpXSRNTdCqLH2jG8-0QI8VnzY'
+// variáveis vindas da Vercel (.env)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
-
